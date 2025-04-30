5c41e4ce30f21e3d78229801e1af93b9132ed347
Albert Nikitin <AlYuryevNikitin@sberbank.ru>
2025-04-30 14:53:56 +0300

    Setup telescope

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,12 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap.set
+local builtin = require("telescope.builtin")
+
 keymap("n", "<leader>w", ":w<CR>")
 keymap("n", "<leader>q", ":q<CR>")
+
+keymap("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+keymap("n", "<leader>fw", builtin.live_grep, { desc = "Live grep" })
+keymap("n", "<leader>fb", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy find in buffer" })
+
