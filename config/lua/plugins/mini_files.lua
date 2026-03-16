f4c2c0167161bf2cfe60987a998b926b75985859
AlbertNikitin <aiunikitin@sberdevices.ru>
2026-03-16 17:30:03 +0300

    delete gitsigns and mini_files go home func on ~

--- config/lua/plugins/mini_files.lua
+++ config/lua/plugins/mini_files.lua
@@ -1,6 +1,23 @@
-require("mini.files").setup({
+local mini_files = require("mini.files")
+
+mini_files.setup({
   windows = {
     preview = true,
     width_preview = 40,
   },
+})
+
+-- Jump to home directory with "~"
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesBufferCreate",
+  callback = function(args)
+    local buf = args.data.buf_id
+
+    vim.keymap.set("n", "~", function()
+      mini_files.open(vim.loop.os_homedir(), false)
+    end, {
+      buffer = buf,
+      desc = "MiniFiles: go to home directory",
+    })
+  end,
 })
